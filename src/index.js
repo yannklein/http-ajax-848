5fca3ba2db59f5591597da5a57c32649953c08a9
Yann Klein <yann.klein@me.com>
2022-06-18 10:59:07 +0900

    lecture done

--- src/index.js
+++ src/index.js
@@ -1,73 +1,87 @@
+// console.log("Hi everyone!!!");
+
 // //////////////////////
 // Rehearsal
 // //////////////////////
 
-// // 1. Select an element (button)
+// 1. Select the button
 // const button = document.querySelector("#click-me");
 
 // // 2. Listen to a click on the button
 // button.addEventListener("click", (event) => {
-//   console.log(event);
-//   // 3. Change the DOM, chnage text, disable
+//   // 3. Change the DOM ( innerText, disabled)
 //   event.currentTarget.innerText = "Loading...";
 //   event.currentTarget.disabled = true;
 // });
-
 
 // //////////////////////
 // HTTP GET request
 // //////////////////////
 
-// 1. Select elements (input, search, list)
+// 1. Select elements: input, search btn, list
 const input = document.querySelector("#keyword");
-const submit = document.querySelector("#submit");
-const results = document.querySelector("#results");
+const search = document.querySelector("#submit");
+const list = document.querySelector("#results");
 
-// 2. Listen to a click on search button
-submit.addEventListener("click", (event) => {
-  event.preventDefault();
+// 2. Listen to a click on search btn
+search.addEventListener("click", (event) => {
   console.log(event);
-  // 2.5 Fetch the OMDBApi
-  const keyword = input.value;
-  const url = `https://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
+  // to prevent default page refresh after click
+  event.preventDefault();
+
+  // 2.5 Fetch the OMDb API, get the input value (=keyword) and search for movies corresponding to the keyword
+  const url = `https://www.omdbapi.com/?s=${input.value}&apikey=adf1f2d7`;
+  console.log("clicked");
   fetch(url)
     .then(response => response.json())
-    .then((data) => {
-      console.log(data.Search);
-      results.innerHTML = "";
+    .then(data => {
+      // console.log(data.Search);
+      // 3. Change the DOM: display the movies in the list!
+      console.log("data arrived!");
+
       const movies = data.Search;
+      list.innerHTML = "";
       movies.forEach((movie) => {
-        // 3. Display the movie data
-        results.insertAdjacentHTML(
-          "beforeend",
+        list.insertAdjacentHTML(
+          "beforeend", 
           `<li class='list-inline-item'>
-            <img src="${movie.Poster}">
+            <img src="${movie.Poster}" alt="" />
             <p>${movie.Title}</p>
           </li>`
         );
       });
     })
-  console.log("Outside the fetch");
+  console.log("after fetch");
 });
-
 
 
 // //////////////////////
 // HTTP POST request
 // //////////////////////
 
-const searchAlgoliaPlaces = (event) => {
-  const url = "https://places-dsn.algolia.net/1/places/query";
-  const bodyData = { query: event.currentTarget.value };
-  fetch(url, {
+const signUp = (event) => {
+  event.preventDefault()
+  const emailValue = document.getElementById("email").value
+  const passwordValue = document.getElementById("password").value
+
+  const data = JSON.stringify(
+    {
+      "email": emailValue, 
+      "password": passwordValue
+    })
+
+  const options = {
     method: "POST",
-    body: JSON.stringify(bodyData)
-  })
+    headers: {"Content-Type": "application/json"},
+    body: data
+  }
+
+  fetch("https://reqres.in/api/register", options)
     .then(response => response.json())
     .then((data) => {
-      console.log(data.hits); // Look at local_names.default
-    });
-};
+      console.log(data)
+    })
+}
 
-const search = document.querySelector("#search");
-search.addEventListener("keyup", searchAlgoliaPlaces);
+const form = document.querySelector("#form")
+form.addEventListener("submit", signUp);
